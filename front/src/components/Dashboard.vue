502be2a0139b34e1cc0398b52a3dc01014c65593
Benchaabane Fahim <f.benchaabane7@gmail.com>
2019-09-24 17:14:35 +0200

    add view by role in dashboard

--- front/src/components/Dashboard.vue
+++ front/src/components/Dashboard.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <h1>Dashboard</h1>
+  <div class="dashboard">
+    <div v-show="getLoggedUser.status === 'loading'">
+      <md-progress-spinner :md-diameter="100" :md-stroke="10" md-mode="indeterminate"></md-progress-spinner>
+    </div>
+    <div v-show="getLoggedUser.status === 'success'">
+      <employee v-show="getLoggedUser.role === 1"/>
+      <manager v-show="getLoggedUser.role === 2"/>
+      <general v-show="getLoggedUser.role === 3"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+  import employee from './Employee.dashboard'
+  import manager from './Manager.dashboard'
+  import general from './General_Manager.dashboard'
 
-}
+  export default {
+    components:{
+      employee,
+      manager,
+      general
+    },
+    computed: {
+      getLoggedUser(){
+        return this.$store.getters['userModule/getLoggedUser']
+      }
+    }
+  }
 </script>
---
+++ front/src/components/Manager.dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Manager dashboard</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
