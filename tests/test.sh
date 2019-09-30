515a663325cf27a5609fdd0f5a94d0782da8bbe7
Yoann <yoann1.abbes@epitech.eu>
2019-09-30 17:02:32 +0200

    test postman

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 
 main() {
     npm install -g newman
-    newman run ./Tests-collection.json -e ./Tests-collection-env.json --timeout 1800000 --timeout-request 1800000 --timeout-script 1800000 --bail
+    newman run ./tests/Tests-collection.json -e ./tests/Tests-collection-env.json --timeout 1800000 --timeout-request 1800000 --timeout-script 1800000 --bail
     status=$?
     exit $status
 }
