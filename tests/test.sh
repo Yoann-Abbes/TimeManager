85c766328984175cfd4a8923aa86409635a88a81
Yoann <yoann1.abbes@epitech.eu>
2019-09-30 16:57:27 +0200

    test postman

---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+main() {
+    npm install -g newman
+    newman run ./Tests-collection.json -e ./Tests-collection-env.json --timeout 1800000 --timeout-request 1800000 --timeout-script 1800000 --bail
+    status=$?
+    exit $status
+}
+
+main
+
+exec docker-compose -f ../back/test.yml run phoenix
