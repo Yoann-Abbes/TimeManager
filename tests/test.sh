7f32fc824ae014d60baa13cafae005e305aaaec9
Yoann <yoann1.abbes@epitech.eu>
2019-09-30 17:29:42 +0200

    test postman

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 main() {
+    mix ecto.reset
     docker-compose up -d
-    until curl --output /dev/null --silent --fail -d '{"user": { "username": "test", "password": "test"}}' -H "Content-Type: application/json" -X POST http://localhost:4000/api/users/sign_in; do
-        echo "API - waiting to start"
-        sleep 2
-    done
     npm install -g newman
-    newman run ./tests/Tests-collection.json -e ./tests/Tests-collection-env.json --timeout 1800000 --timeout-request 1800000 --timeout-script 1800000 --bail
+    newman run ./tests/Tests-collection.json -e ./tests/Tests-collection-env.json --timeout 18000000 --timeout-request 18000000 --timeout-script 18000000 --bail
     status=$?
     exit $status
 }
